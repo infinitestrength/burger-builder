7edd5e635e05a2cd36911475465d3576be959b34
Srikrishna karteek marripati <krishnakart36ik@gmail.com>
2018-06-10 15:30:11 +0530

    basic functionality completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './hoc/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Layout><p>initial setup...</p></Layout>
+        <Layout><BurgerBuilder/></Layout>
+
       </div>
     );
   }
